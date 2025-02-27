3075d13c6ba4e0e6d87ea415c3d2ca5576b0b666
myamusashi <jinxprogilang666@gmail.com>
2025-02-28 04:49:07 +0700

    Fix: applicationName missing

--- zen-browser-unwrapped.nix
+++ zen-browser-unwrapped.nix
@@ -19,72 +19,74 @@
   url,
   hash,
   ...
-}:
-let
-  policies = {
-    DisableAppUpdate = true;
-  } // config.zen.policies or { };
+}: let
+  policies =
+    {
+      DisableAppUpdate = true;
+    }
+    // config.zen.policies or {};
 
-  policiesJson = writeText "firefox-policies.json" (builtins.toJSON { inherit policies; });
+  policiesJson = writeText "firefox-policies.json" (builtins.toJSON {inherit policies;});
 in
-stdenv.mkDerivation (finalAttrs: {
-  inherit version;
-  pname = "zen-browser-unwrapped";
+  stdenv.mkDerivation (finalAttrs: {
+    inherit version;
+    pname = "zen-browser-unwrapped";
+    applicationName = "Zen Browser";
 
-  src = fetchurl {
-    inherit url hash;
-  };
+    src = fetchurl {
+      inherit url hash;
+    };
 
-  nativeBuildInputs = [
-    wrapGAppsHook3
-    autoPatchelfHook
-    patchelfUnstable
-  ];
+    nativeBuildInputs = [
+      wrapGAppsHook3
+      autoPatchelfHook
+      patchelfUnstable
+    ];
 
-  buildInputs = [
-    gtk3
-    alsa-lib
-    adwaita-icon-theme
-    dbus-glib
-    libXtst
-  ];
+    buildInputs = [
+      gtk3
+      alsa-lib
+      adwaita-icon-theme
+      dbus-glib
+      libXtst
+    ];
 
-  runtimeDependencies = [
-    curl
-    libva.out
-    pciutils
-  ];
+    runtimeDependencies = [
+      curl
+      libva.out
+      pciutils
+    ];
 
-  appendRunpaths = [
-    "${pipewire}/lib"
-  ];
+    appendRunpaths = [
+      "${pipewire}/lib"
+    ];
 
-  installPhase = ''
-    mkdir -p "$prefix/lib/zen-${version}"
-    cp -r * "$prefix/lib/zen-${version}"
+    installPhase = ''
+      mkdir -p "$prefix/lib/zen-${version}"
+      cp -r * "$prefix/lib/zen-${version}"
 
-    mkdir -p $out/bin
-    ln -s "$prefix/lib/zen-${version}/zen" $out/bin/zen
+      mkdir -p $out/bin
+      ln -s "$prefix/lib/zen-${version}/zen" $out/bin/zen
 
-    mkdir -p "$out/lib/zen-${version}/distribution"
-    ln -s ${policiesJson} "$out/lib/zen-${version}/distribution/policies.json"
-  '';
-
-  patchelfFlags = [ "--no-clobber-old-sections" ];
-
-  meta = {
-    mainProgram = "zen";
-    description = ''
-      Zen is a privacy-focused browser that blocks trackers, ads, and other unwanted content while offering the best browsing experience!
+      mkdir -p "$out/lib/zen-${version}/distribution"
+      ln -s ${policiesJson} "$out/lib/zen-${version}/distribution/policies.json"
     '';
-  };
 
-  passthru = {
-    inherit gtk3;
+    patchelfFlags = ["--no-clobber-old-sections"];
 
-    libName = "zen-${version}";
-    binaryName = finalAttrs.meta.mainProgram;
-    gssSupport = true;
-    ffmpegSupport = true;
-  };
-})
+    meta = {
+      mainProgram = "zen";
+      description = ''
+        Zen is a privacy-focused browser that blocks trackers, ads, and other unwanted content while offering the best browsing experience!
+      '';
+    };
+
+    passthru = {
+      inherit gtk3;
+
+      libName = "zen-${version}";
+      binaryName = finalAttrs.meta.mainProgram;
+      gssSupport = true;
+      ffmpegSupport = true;
+    };
+  })
